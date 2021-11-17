d8356870ec8e79177eb81e11b96c9b58226ded5d
James Coulter <seniordev1994128@gmail.com>
2021-11-16 18:40:47 -0800

    remove js files for api

--- Analisys Product Article Recommendations/src/api/contactNotes.js
+++
@@ -1,9 +0,0 @@
-import ApiClient from './ApiClient';
-
-class ContactNotes extends ApiClient {
-  constructor() {
-    super('contact_notes', { accountScoped: true });
-  }
-}
-
-export default new ContactNotes();
